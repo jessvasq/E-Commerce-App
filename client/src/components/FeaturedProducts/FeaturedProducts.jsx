0b1554497c3840e2ce6e62e60512f9787e021876
User Name <jessnsq.codes@gmail.com>
2024-04-03 19:55:10 -0400

    fetching data

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './FeaturedProducts.scss';
 import Card from '../Card/Card';
+import axios from 'axios';
 
 const FeaturedProducts = ({type}) => {
     const data = [
@@ -76,6 +77,25 @@ const FeaturedProducts = ({type}) => {
 
     ]
 
+    const [products, setProducts] = useState([]) 
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            try{
+                const data = await axios.get(process.env.REACT_APP_API_URL + '/products', 
+                {
+                    headers: {
+                        Authorization : "bearer " + process.env.REACT_APP_API_TOKEN,
+                    },
+                }
+                );
+                console.log(data);
+            } catch (err) {
+                console.log(err)
+            }
+        };
+        fetchData();
+    }, [])
 
     return (
         <div className='featuredProducts'>
